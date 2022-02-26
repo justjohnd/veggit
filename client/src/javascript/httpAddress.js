c4e0395b61b0ddd8bb15549b8be853ed2f2eb518
johnd <justjohnd@gmail.com>
2022-02-26 15:49:49 +0900

    Change httpAddress

--- client/src/javascript/httpAddress.js
+++ client/src/javascript/httpAddress.js
@@ -1,6 +1,8 @@
-const local = 'http://localhost:5000';
-const production = 'https://secret-springs-85430.herokuapp.com/';
+const addresses = {
+  local: 'http://localhost:5000',
+  production: 'https://secret-springs-85430.herokuapp.com/'
+}
 
-const httpAddress = production;
+const httpAddress = addresses.production;
 
 export default httpAddress;
