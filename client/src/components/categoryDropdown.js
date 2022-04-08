cdd2b32113b92616684a95937618deed3bc9e36a
johnd <justjohnd@gmail.com>
2022-04-08 16:07:17 +0900

    Add expiry to local storage

--- client/src/components/categoryDropdown.js
+++ client/src/components/categoryDropdown.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { categories } from '../javascript/categories.js';
+
 import { default as ReactSelect } from 'react-select';
 import { components } from 'react-select';
 
+import { categories } from '../javascript/categories.js';
+
 const Option = (props) => {
-  
   return (
     <div>
       <components.Option {...props}>
@@ -19,32 +20,29 @@ const Option = (props) => {
   );
 };
 
-export default function CategoryDropdown(props)  {
-
- function handleChange(selected) {
-    // setOptionSelected(selected);
+export default function CategoryDropdown(props) {
+  function handleChange(selected) {
     props.categoriesCallback(selected);
-  };
-
-    return (
-      <span
-        className="d-inline-block mb-3"
-        data-toggle="popover"
-        data-trigger="focus"
-        data-content="Please selecet account(s)"
-      >
-        <ReactSelect
-          options={categories}
-          isMulti
-          closeMenuOnSelect={false}
-          hideSelectedOptions={false}
-          components={{
-            Option,
-          }}
-          onChange={handleChange}
-          allowSelectAll={true}
-          value={props.selectedCategories}
-        />
-      </span>
-    );
   }
+
+  return (
+    <span
+      className="d-inline-block category-dropdown mb-3"
+      data-toggle="popover"
+      data-trigger="focus"
+      data-content="Please selecet account(s)"
+    >
+      <ReactSelect
+        options={categories}
+        isMulti
+        closeMenuOnSelect={false}
+        hideSelectedOptions={false}
+        components={{
+          Option,
+        }}
+        onChange={handleChange}
+        allowSelectAll={true}
+      />
+    </span>
+  );
+}
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -5,12 +5,14 @@ import LocalDiningIcon from '@mui/icons-material/LocalDining';
 import CreateIcon from '@mui/icons-material/Create';
 import WebIcon from '@mui/icons-material/Web';
 import LogoutIcon from '@mui/icons-material/Logout';
-
 // Note that the bootstrap import below is necessary for navbar behavior
 import 'bootstrap/dist/css/bootstrap.css';
 import bootstrap from 'bootstrap';
 
 import '../index.css';
+
+import { getWithExpiry } from '../hooks/localStorageWithExpiry';
+
 import UrlSearch from './urlSearch';
 
 // Here, we display our Navbar
@@ -41,33 +43,35 @@ const Navbar = (props) => {
           <span className="custom-toggler navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          {!props.sessionExpired && localStorage.getItem('authToken') ? (
+          {getWithExpiry('authToken') ? (
             <div className="mt-3">
               <ul className="navbar-nav ml-auto">
-                <li className="nav-item me-md-4">
+                <li className="nav-item mb-4 me-md-4">
+                  <WebIcon className="d-lg-none me-3 align-middle" />
                   <UrlSearch
                     className="nav-link"
                     loaderCallback={props.loaderCallback}
                   />
                 </li>
-                <li className="nav-item me-md-4">
+                <li className="nav-item mb-4 me-md-4">
                   <NavLink className="nav-link" to="/create">
+                    <CreateIcon className="d-lg-none me-3 align-middle" />
                     new
                   </NavLink>
                 </li>
-                <li className="nav-item me-md-4">
+                <li className="nav-item mb-4 me-md-4">
                   <NavLink className="nav-link" to="/private">
                     <LocalDiningIcon className="d-lg-none me-3 align-middle" />
                     Cookbook
                   </NavLink>
                 </li>
-                <li className="nav-item me-md-4">
+                <li className="nav-item mb-4 me-md-4">
+                  <LogoutIcon className="d-lg-none me-3 align-middle" />
                   <Link
                     className="nav-link"
                     to="login"
                     onClick={() => {
                       handleLogout();
-                      props.sessionExpiredCallback(true);
                     }}
                   >
                     Logout
